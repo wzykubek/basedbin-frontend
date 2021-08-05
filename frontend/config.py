59b27a42bcdf3f2f4881283161250eaca2bb6dde
samedamci <samedamci@disroot.org>
2021-08-05 20:19:50 +0200

    Update config.py to read environment variables

--- frontend/config.py
+++ frontend/config.py
@@ -1,1 +1,9 @@
-HOST_URL = "http://localhost:8080"
+from os import getenv
+
+server_hostname = getenv("SERVER_HOSTNAME")
+if getenv("SERVER_PORT"):
+    server_port = getenv("SERVER_PORT")
+else:
+    server_port = "80"
+
+HOST_URL = f"http://{server_hostname}:{server_port}"
